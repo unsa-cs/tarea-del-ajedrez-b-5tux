787cb26bb413ca3113aeac79b8512ace796b73fc
LuisRamos <luisenramos0805@gmail.com>
2024-10-03 23:23:55 -0500

    Experimentando con la funcion up, nos damos cuenta que coloca el primer parametro encima del segundo parametro

--- prob3.c
+++ prob3.c
@@ -5,6 +5,8 @@ void display(){
     char** blackSquare = reverse(whiteSquare);
     char** whiteandblack = join(whiteSquare, blackSquare);
     char** blackandwhite = reverse(whiteandblack);
-    char** col = repeatV(blackandwhite, 4);
-    interpreter(col);
+    char** row1 = repeatH(blackandwhite, 4);
+    char** row2 = repeatH(whiteandblack, 4);
+    char** mergeRow = up(row1, row2);
+    interpreter(mergeRow);
 }
