b713c9e4a6ddcf66aa9963a85d82f08cbc456f52
piero <pieroadriano2018@gmail.com>
2024-10-05 18:19:39 -0500

    Se genero 2 filas de tablero sin figuras adicionales, y con la funcion up ,se unio con las 2 filas de las figuras

--- prob06.c
+++ prob06.c
@@ -6,6 +6,7 @@ void display(){
     char** blackAndWhite = join(blackSquare, whiteSquare);
     char** row = repeatH(blackAndWhite, 4);
     char** reverseRow = reverse(row);
+    char** unionRows = up(row, reverseRow);
 
     char** rookKnight = join(rook, knight);
     char** bishopQueen = join(bishop, queen);
@@ -21,6 +22,8 @@ void display(){
     char** rowPawn = repeatH(pawn, 8);
     char** rowPawnOnSquares = superImpose(rowPawn, reverseRow);
 
-    char** whiteSideFigures = up(rowUnionFigures, rowPawnOnSquares); 
-    interpreter(whiteSideFigures);
+    char** whiteSideFigures = up(rowUnionFigures, rowPawnOnSquares);
+    char** halfChessBoard = up(whiteSideFigures, unionRows);
+
+    interpreter(halfChessBoard);
 }
