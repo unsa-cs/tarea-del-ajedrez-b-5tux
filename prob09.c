3f565fb72e24faa63c76901c9d3abb551b784c36
Anderson Carpio <acarpioch@unsa.edu.pe>
2024-10-06 11:19:13 -0500

    Finalizando la fila 1, utilizando la funcion join para unir lo obtenido en el experimento anterior con el lado izquierdo de cuadros blancos y negros

--- prob09.c
+++ prob09.c
@@ -10,7 +10,8 @@ void display(){
     char** blackKingOnBlackSquare = superImpose(reverse(king), blackSquare);
     char** blackQueenRow1 = join(blackQueenOnWhiteSquare, blackSquare);
     char** blackKingRow1 = join(whiteSquare, blackKingOnBlackSquare);
-    char** unionTest = join(blackQueenRow1, blackKingRow1);
+    char** whiteAndBlackRow1 = repeatH(whiteAndBlack, 2);
+    char** completeRow1 = join(whiteAndBlackRow1, join(blackQueenRow1, blackKingRow1));
 
-    interpreter(unionTest);
+    interpreter(completeRow1);
 }
