33c04227ed22c138fcf5c631230841cd0dc6c898
Anderson Carpio <acarpioch@unsa.edu.pe>
2024-10-06 11:14:44 -0500

    Creamos la primera parte de la primera fila, utilizando la funcion reverse en queen y king, funcion superImpose para colocarlos encima de los cuadros y la funcion join para unir todo

---
+++ prob09.c
@@ -0,0 +1,16 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+    char** blackSquare = reverse(whiteSquare);
+    char** whiteAndBlack = join(whiteSquare, blackSquare);
+    char** blackAndWhite = reverse(whiteAndBlack);
+
+    char** blackQueenOnWhiteSquare = superImpose(reverse(queen), whiteSquare);
+    char** blackKingOnBlackSquare = superImpose(reverse(king), blackSquare);
+    char** blackQueenRow1 = join(blackQueenOnWhiteSquare, blackSquare);
+    char** blackKingRow1 = join(whiteSquare, blackKingOnBlackSquare);
+    char** unionTest = join(blackQueenRow1, blackKingRow1);
+
+    interpreter(unionTest);
+}
