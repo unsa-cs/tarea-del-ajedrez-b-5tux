2797e213dc56f35f0c447a12f9964b862481efe4
Anderson Carpio <acarpioch@unsa.edu.pe>
2024-10-05 19:25:48 -0500

    Creamos el tablero de 2x2 utilizando las funciones join y up. Luego utilizamos la funcion superImpose para colocar los 4 caballos con rotaciones distintas encima del tablero

--- prob07.c
+++ prob07.c
@@ -2,14 +2,20 @@
 #include "figures.h"
 
 void display(){
+    char** blackSquare = reverse(whiteSquare);
+    char** blackAndWhite = join(blackSquare, whiteSquare);
+    char** whiteAndBlack = reverse(blackAndWhite);
+    char** emptyBoard = up(blackAndWhite, whiteAndBlack);  
+
     char** knight90 = rotateR(knight);
     char** knight180 = rotateR(knight90);
     char** knight270 = rotateR(knight180);
 
     char** upperKnights = join(knight, knight90);
     char** lowerKnights = join(knight180, knight270);
-    
     char** fourKnights = up(upperKnights, lowerKnights);
 
-    interpreter(fourKnights);
+    char** knightBoard = superImpose(fourKnights, emptyBoard);
+
+    interpreter(knightBoard);
 }
